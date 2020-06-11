17f4a6f8ca85c4bda76cec881f2cf0636e1077e2
Rafkat <55236722+Rafkat@users.noreply.github.com>
2020-06-11 12:03:07 +0300

    fourth commit

--- server/user.py
+++ server/user.py
@@ -1,15 +1,44 @@
 import sqlite3
-from flask_restx import Namespace, Resource, reqparse, inputs
+
+from flask import request
+from flask_restx import Namespace, Resource
+
 api = Namespace('users', description='User resources')
 
-#---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
 @api.route('/users/<int:user_id>')
 @api.param('user_id', description='User ID')
 class User(Resource):
     def get(self, user_id):
         db = sqlite3.connect('db.sqlite')
         c = db.cursor()
-        c.execute("select name from appuser where id = ?", (user_id,))
-        for name, in c:
-            return name
+        c.execute("select name, id from appuser where id = ?", (user_id,))
+        for name, id in c:
+            return {"id": id, "name": name}
 
+
+@api.route('/users')
+class Users(Resource):
+    def get(self):
+        db = sqlite3.connect('db.sqlite')
+        c = db.cursor()
+        c.execute("select name, id from appuser")
+        users = []
+        for name, id in c:
+            users.append({"id": id, "name": name})
+        return users
+
+    def post(self):
+        data = request.get_json()
+        db = sqlite3.connect('db.sqlite')
+        c = db.cursor()
+        c.execute("select id from appuser order by id desc limit 1")
+        id = None
+        for ui, in c:
+            id = ui+1
+        name = data.get('name')
+        data["id"] = id
+        c.execute("insert into appuser (id, name) values(?,?)", (id, name))
+        db.commit()
+        return data
